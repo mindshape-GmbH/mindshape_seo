6fc62ff35f5c752176a98d0eeee89e41e5d158bd
Daniel Dorndorf <dorndorf@mindshape.de>
2016-08-01 14:51:18 +0200

    [TASK] Add PHP 5.5 dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,6 +16,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'typo3' => '7.6.0-7.6.99',
+            'php' => '5.5.0'
         ),
         'conflicts' => array(),
         'suggests' => array(),
