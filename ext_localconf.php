5309e2e4ebe2eca1ddffbc379f27196fc509cc02
Daniel Dorndorf <dorndorf@mindshape.de>
2016-11-24 09:24:41 +0100

    [TASK] Add hreflang hook for typolink

--- ext_localconf.php
+++ ext_localconf.php
@@ -15,6 +15,7 @@ if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('news')) {
     $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['mindshape_seo']['sitemap_preRendering'][] = \Mindshape\MindshapeSeo\Hook\NewsSitemapHook::class . '->sitemapPreRendering';
 }
 
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['typoLink_PostProc']['realurl'] = \Mindshape\MindshapeSeo\Hook\TypoLinkHook::class . '->postProcessEncodedUrl';
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][] = \Mindshape\MindshapeSeo\Hook\RenderPreProcessHook::class . '->main';
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerTypeConverter(\Mindshape\MindshapeSeo\Property\TypeConverter\UploadedFileReferenceConverter::class);
 
