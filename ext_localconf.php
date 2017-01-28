07a66d45d181d5b90b299de06166814dff4d868f
Daniel Dorndorf <dorndorf@featdd.de>
2017-01-28 15:40:50 +0100

    [TASK] Refactor realurl configuration to respect its autoconfigurator

--- ext_localconf.php
+++ ext_localconf.php
@@ -17,43 +17,12 @@ if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('news')) {
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['typoLink_PostProc']['realurl'] = \Mindshape\MindshapeSeo\Hook\TypoLinkHook::class . '->postProcessEncodedUrl';
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][] = \Mindshape\MindshapeSeo\Hook\RenderPreProcessHook::class . '->main';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration'][] = \Mindshape\MindshapeSeo\Hook\RealurlHook::class . '->addConfiguration';
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerTypeConverter(\Mindshape\MindshapeSeo\Property\TypeConverter\UploadedFileReferenceConverter::class);
 
-if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('realurl')) {
-    if (!array_key_exists('realurl', $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'])) {
-        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl'] = array();
-    }
-
-    if (!array_key_exists('_DEFAULT', $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl'])) {
-        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT'] = array();
-    }
-
-    if (!array_key_exists('fileName', $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT'])) {
-        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT']['fileName'] = array();
-    }
-
-    if (!array_key_exists('index', $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT']['fileName'])) {
-        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT']['fileName']['index'] = array();
-    }
-
-    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT']['fileName']['index'] = array_merge(
-        array(
-            'sitemap.xml' => array(
-                'keyValues' => array(
-                    'type' => 19371,
-                ),
-            ),
-            'sitemap-index.xml' => array(
-                'keyValues' => array(
-                    'type' => 19372,
-                ),
-            ),
-            'sitemap-image.xml' => array(
-                'keyValues' => array(
-                    'type' => 19373,
-                ),
-            ),
-        ),
-        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT']['fileName']['index']
-    );
+if (
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('realurl') &&
+    true === array_key_exists('realurl', $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'])
+) {
+    \Mindshape\MindshapeSeo\Hook\RealurlHook::checkConfiguration();
 }
