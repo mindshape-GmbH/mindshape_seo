4b38e3d3cf07d6843b60dc49c15cde783931b56a
Can Karadag <karadag@mindshape.de>
2020-07-20 11:28:04 +0200

    [TASK] change included typoscript file names

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,11 +3,11 @@ defined('TYPO3_MODE') || die('Access denied.');
 
 call_user_func(function () {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants(
-        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:mindshape_seo/Configuration/TypoScript/constants.txt">'
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:mindshape_seo/Configuration/TypoScript/constants.typoscript">'
     );
 
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
-        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:mindshape_seo/Configuration/TypoScript/setup.txt">'
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:mindshape_seo/Configuration/TypoScript/setup.typoscript">'
     );
 
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][] = \Mindshape\MindshapeSeo\Hook\RenderPreProcessHook::class . '->main';
@@ -20,5 +20,12 @@ call_user_func(function () {
             ->registerImplementation(\TYPO3\CMS\Extbase\Domain\Model\FileReference::class,
                 \Mindshape\MindshapeSeo\Domain\Model\FileReference::class);
     }
+
+    // Register a node in ext_localconf.php
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1594739604] = [
+        'nodeName' => 'googlePreview',
+        'priority' => 40,
+        'class' => \Mindshape\MindshapeSeo\Backend\Form\Element\GooglePreviewElement::class,
+    ];
 
 });
